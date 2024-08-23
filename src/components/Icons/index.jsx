d06c4cd7388b503528fe01a4d85de11fe0e93688
voxuantu81 <tu.vo@bmbsoft.com.vn>
2024-08-23 15:07:32 +0700

    make video section for home page

--- src/components/Icons/index.jsx
+++ src/components/Icons/index.jsx
@@ -1,11 +1,17 @@
 import ArrowRight from "./ArrowRight";
+import Close from "./Close";
 import Heart from "./Heart";
 import Menu from "./Menu";
+import ArrowRightLong from "./ArrowRightLong";
+import Diamond from "./Diamond";
 
 const Icons = {
   Menu,
   ArrowRight,
   Heart,
+  Close,
+  ArrowRightLong,
+  Diamond,
 };
 
 export default Icons;
